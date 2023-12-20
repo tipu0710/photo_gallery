e051d684304f82f276b6631a0a3dbdbebf38eeb7
Tipu Sultan <contact.tipusultan@gmail.com>
2023-12-20 23:30:04 +0600

    feat(Screen): :art: Photo description
    Dummy photo description added

--- lib/screen/photoDetails/photo_details.dart
+++ lib/screen/photoDetails/photo_details.dart
@@ -85,11 +85,23 @@ class _PhotoDetailsState extends State<PhotoDetails> {
             const Padding(
               padding: EdgeInsets.symmetric(horizontal: 8.0),
               child: Text(
-                "Related Photos:",
+                "Photo Description:",
                 style: TextStyle(
                   color: Colors.black,
                   fontSize: 16,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 42, 33, 33),
+                  fontSize: 14,
+                  fontWeight: FontWeight.normal,
                 ),
               ),
             ),
